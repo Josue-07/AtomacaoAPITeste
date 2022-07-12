ff4eb563b1f7a228d1bd4c72057b06425055341a
Josue-07 <josuelima875@gmail.com>
2022-07-11 23:59:14 -0300

    first commit

---
+++ src/test/java/br/ce/wcaquino/rest/core/BaseTests.java
@@ -0,0 +1,29 @@
+package br.ce.wcaquino.rest.core;
+
+import org.hamcrest.Matchers;
+import org.junit.BeforeClass;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+
+public class BaseTests implements Constantes {
+
+	@BeforeClass
+	public static void setup() {
+		RestAssured.baseURI = APP_BASE_URL;
+		RestAssured.port = APP_PORTA;
+		RestAssured.basePath = APP_BASE_PATH;
+		
+		RequestSpecBuilder requBuild = new RequestSpecBuilder();
+		requBuild.setContentType(APP_CONTENT_TYPE);
+		RestAssured.requestSpecification = requBuild.build();
+		
+		ResponseSpecBuilder respBuild = new ResponseSpecBuilder();
+		respBuild.expectResponseTime(Matchers.lessThan(MAX_TIMEOUT));
+		RestAssured.responseSpecification = respBuild.build();
+		
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		
+	}
+}
